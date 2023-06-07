fc2a19cf2c93ef63e829719fef91895b32e1f022
Philip Carlsson <philipcarlsson@hotmail.com>
2023-06-07 21:31:03 +0200

    changing placeholder categories' id to same as in api

--- src/queries/get-categories/utils.ts
+++ src/queries/get-categories/utils.ts
@@ -1,19 +1,19 @@
 export const placerHolderCategories = {
   trivia_categories: [
     {
-      id: 99,
+      id: 9,
       name: "General Knowledge",
     },
     {
-      id: 100,
+      id: 10,
       name: "Entertainment: Books",
     },
     {
-      id: 101,
+      id: 13,
       name: "Entertainment: Musicals & Theatres",
     },
     {
-      id: 102,
+      id: 31,
       name: "Entertainment: Japanese Anime & Manga",
     },
   ],
